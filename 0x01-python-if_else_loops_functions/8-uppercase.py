546502933174f19e9b9a214aba7d0e988cddf1a4
liyu <liyuethiop@gmail.com>
2022-11-30 01:05:45 -0800

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    ch2 = ''
+    for ch in str:
+        if ord(ch) in range(97, 123):
+            ch2 += chr(ord(ch) - 32)
+        else:
+            ch2 += ch
+    print(ch2)
