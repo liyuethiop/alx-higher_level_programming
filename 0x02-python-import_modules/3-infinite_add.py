ea24758a9f5649d7162440e4ad326080b72797fa
liyu <liyuethiop@gmail.com>
2022-12-07 10:42:59 -0800

    tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                                  2,1           All
-
+    sum = 0
+    if len(sys.argv) is 1:
+        print("{}".format(sum))
+    elif len(sys.argv) > 1:
+        for x in range(1, len(sys.argv)):
+            sum = sum + int(sys.argv[x])
+        print("{}".format(sum))
