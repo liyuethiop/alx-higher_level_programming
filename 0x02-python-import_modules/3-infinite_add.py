ba9f7bc3c6c62aae02e036458af54edc4c60eec6
liyu <liyuethiop@gmail.com>
2022-12-07 10:23:56 -0800

    tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                  2,1           All
+
